830deb4899662a5baaedb096cb111cccfd559d1d
Matteus <tolfx@tolfix.com>
2021-07-26 22:17:21 +0200

    Modified test

--- test/Config.test.ts
+++ test/Config.test.ts
@@ -1,7 +1,11 @@
-import { Discord_Token } from "../src/Config";
+import { Discord_Token, Prefix } from "../src/Config";
 
 describe("Checking envs", () => {
-    it("should be empty", () => {
+    it("should be equal | discord_token", () => {
         expect(Discord_Token).toEqual(process.env.DISCORD_TOKEN ?? "")
+    });
+
+    it("should be equal | Prefix", () => {
+        expect(Prefix).toEqual(process.env.PREFIX ?? "tib ")
     });
 })
