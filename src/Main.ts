bb2846948f3a920bb9cdb94cf100109b6c966179
Matteus <tolfx@tolfix.com>
2021-07-26 22:12:08 +0200

    Modified < Main >

--- src/Main.ts
+++ src/Main.ts
@@ -1,1 +1,19 @@
-export {};
+import { Client } from "discord.js";
+import { Discord_Token } from "./Config";
+
+const client = new Client();
+
+client.on("ready", () => {
+    client.user?.setPresence({
+
+        status: "dnd",
+
+        activity: {
+            type: "STREAMING",
+            name: "TIB"
+        }
+
+    })
+});
+
+client.login(Discord_Token);
