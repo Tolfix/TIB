ff84242c6948c31fe8f7cf3217b4c5ba25ff554a
Matteus <tolfx@tolfix.com>
2021-07-27 20:31:10 +0200

    new Discord

---
+++ src/Discord/Discord.ts
@@ -0,0 +1,30 @@
+
+import { Client } from "discord.js";
+import { Discord_Token, Prefix } from "../Config";
+import ExpressClient from "../Express/Express";
+
+export default class DiscordClient
+{
+    private client: Client;
+
+    constructor()
+    {
+        this.client = new Client();
+        
+        new ExpressClient(this.client);
+
+        this.client.on("ready", () => {
+            this.client.user?.setPresence({
+
+                activity: {
+                    type: "STREAMING",
+                    name: `TIB | ${Prefix}help`
+                }
+
+            })
+        });
+
+        this.client.login(Discord_Token);
+    }
+
+}
