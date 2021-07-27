3fcb393e79fb01d9690c2a7a1df297365e2617ed
Matteus <tolfx@tolfix.com>
2021-07-27 20:30:29 +0200

    Small changes

--- test/Config.test.ts
+++ test/Config.test.ts
@@ -1,11 +1,19 @@
-import { Discord_Token, Prefix } from "../src/Config";
+import { Discord_Token, Prefix, Database_Mongo_URI, Express_Port } from "../src/Config";
 
 describe("Checking envs", () => {
     it("should be equal | discord_token", () => {
-        expect(Discord_Token).toEqual(process.env.DISCORD_TOKEN ?? "")
+        expect(Discord_Token).toEqual(process.env.DISCORD_TOKEN ?? "");
     });
 
     it("should be equal | Prefix", () => {
-        expect(Prefix).toEqual(process.env.PREFIX ?? "tib ")
+        expect(Prefix).toEqual(process.env.PREFIX ?? "tib ");
+    });
+
+    it("should be equal | Database_Mongo_URI", () => {
+        expect(Database_Mongo_URI).toEqual(process.env.DB_MONGO_URI ?? "mongodb://localhost/tib");
+    });
+
+    it("should be equal | Express_Port", () => {
+        expect(Express_Port).toEqual(process.env.EXPRESS_PORT ?? 8080);
     });
 })
