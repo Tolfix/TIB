b6ed00993d368a8f6cbd1d2007acd3ce9d20f252
Matteus <tolfx@tolfix.com>
2021-07-27 20:31:05 +0200

    new Mongo

---
+++ src/Database/Mongo.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { Database_Mongo_URI } from "../Config";
+import log from "../Lib/Logger";
+
+export default class Mongo_Database
+{
+    private db;
+
+    constructor()
+    {
+        mongoose.connect(Database_Mongo_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        
+        this.db = mongoose.connection;
+
+        this.db.on('error', (error: any) => {
+            log.error(`A error accured for the database`, error);
+        });
+    
+        this.db.on('disconnected', () => {
+            log.error(`Lost connection to the database, shutting down.`)
+            process.exit(1);
+        })
+    
+        this.db.once('open', () => {
+            log.verbos(`Database opened`)
+        });
+    }
+}
