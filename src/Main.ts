a42f6b536e18b73dadb1c725286895cbf730121a
Matteus <tolfx@tolfix.com>
2021-07-27 20:30:58 +0200

    Modified < Main >

--- src/Main.ts
+++ src/Main.ts
@@ -1,17 +1,6 @@
-import { Client } from "discord.js";
-import { Discord_Token, Prefix } from "./Config";
+require("dotenv").config();
+import Mongo_Database from "./Database/Mongo";
+import DiscordClient from "./Discord/Discord";
 
-const client = new Client();
-
-client.on("ready", () => {
-    client.user?.setPresence({
-
-        activity: {
-            type: "STREAMING",
-            name: `TIB | ${Prefix}help`
-        }
-
-    })
-});
-
-client.login(Discord_Token);
+new DiscordClient();
+new Mongo_Database();
