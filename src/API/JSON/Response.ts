b804ce1e159d9dbef75b3dacea0bbd2d0a0855c2
Tolfix <matteus12122@gmail.com>
2021-07-28 15:31:31 +0200

    API Responses

---
+++ src/API/JSON/Response.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from 'express';
+
+export function API_Error(response: Object, status?: number)
+{
+    return (res: Response) =>
+    {
+        let result = {
+            status: status ? status : 400,
+            type: "error",
+            message: response
+        };
+        return res.status(status ?? 400).json(result);
+    }
+}
+
+export function API_Success(response: Object, status?: number)
+{
+    return (res: Response) =>
+    {
+        let result = {
+            status: status ? status : 200,
+            type: "success",
+            message: response
+        };
+        return res.status(status ?? 200).json(result);
+    }
+}
