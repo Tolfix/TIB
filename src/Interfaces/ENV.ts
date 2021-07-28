8803aa1f9f3abc2a4604c1ddbd0ca622d175c28c
Tolfix <matteus12122@gmail.com>
2021-07-28 14:04:57 +0200

    New < ENV.ts >

---
+++ src/Interfaces/ENV.ts
@@ -0,0 +1,18 @@
+export interface IENV
+{
+    DISCORD_TOKEN: string;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
+    PREFIX: string;
+    
+    EXPRESS_PORT: string;
+    FQDN: string;
+    HTTP: string;
+    EXPRESS_SESSION_SECRET: string;
+    DB_MONGO_URI: string;
+
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+
+    DEBUG?: string;
+}
