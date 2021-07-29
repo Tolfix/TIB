769ca72deb10fbf93011a92eedd687d713c20e8a
Tolfix <matteus12122@gmail.com>
2021-07-29 14:30:32 +0200

    New < SlashReply.ts >

---
+++ src/Lib/Discord/SlashReply.ts
@@ -0,0 +1,44 @@
+import { APIMessage, Client } from "discord.js"
+import { Interaction } from "slash-commands"
+
+export default class SlashReply
+{
+    private client: Client;
+    private interaction: Interaction;
+    constructor(client: Client, interaction: Interaction)
+    {
+        this.client = client;
+        this.interaction = interaction;
+    }
+
+    private createAPIMessage = async (content: any) => {
+        const { data, files } = await APIMessage.create(
+            //@ts-ignore
+            this.client?.channels?.resolve(this.interaction?.channel_id),
+        content
+        )
+            .resolveData()
+            .resolveFiles()
+    
+        return { ...data, files }
+    }
+    
+    public reply = async ( response: any) => {
+        let data = {
+            content: response,
+        }
+    
+        if (typeof response === 'object') {
+            //@ts-ignore
+            data = await this.createAPIMessage(response)
+        }
+    
+        //@ts-ignore
+        this.client.api.interactions(this.interaction.id, this.interaction.token).callback.post({
+            data: {
+            type: 4,
+            data,
+            },
+        })
+    }
+};
