9b25e2719e230a06d3d762840906767ecbe2d601
Matteus <tolfx@tolfix.com>
2021-07-29 20:27:40 +0200

    New < QuickButton.ts >

---
+++ src/Lib/Discord/QuickButton.ts
@@ -0,0 +1,7 @@
+import CreateButton from "./CreateButton";
+
+export const Quick_Link_Github_Button = CreateButton({
+    name: "Link discord/github",
+    emoji: `🔗`,
+    url: "https://tib.tolfix.com/oauth2/link"
+})
