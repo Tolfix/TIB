44b3e11e3330f033a154f3cc8df985f5444c48f5
Matteus <tolfx@tolfix.com>
2021-07-29 20:26:48 +0200

    Fixed syntax error

--- src/Config.ts
+++ src/Config.ts
@@ -6,7 +6,7 @@ export const Discord_Client_Id = process.env.DISCORD_CLIENT_ID ?? "";
 export const Discord_Client_Secret = process.env.DISCORD_CLIENT_SECRET ?? "";
 export const Discord_Guild_Id = "833438897484595230";
 export const Discord_Public_Key = "b7e1889432f44a4f8c88bcacfbfbe145e45a4fe4138fcd24cc1a6b315f000999";
-export const Discord_Ticker_Parent_Id = "870276061261336616"
+export const Discord_Ticket_Parent_Id = "870276061261336616"
 export const Prefix = process.env.PREFIX ?? "tib ";
 
 export const Express_Port = process.env.EXPRESS_PORT ?? 8080;
