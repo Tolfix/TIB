32f013693260342b577dea10c2b95413b8e4d534
Tolfix <matteus12122@gmail.com>
2021-07-29 17:23:47 +0200

    More caching

--- src/Cache/Cache.ts
+++ src/Cache/Cache.ts
@@ -1,5 +1,9 @@
+import fetch from "node-fetch";
+import { Github_API, Github_Client_Secret, Github_Org } from "../Config";
 import UserModel from "../Database/Schemes/User";
 import { IC_User } from "../Interfaces/Cache/Cache_User";
+import { Contributor } from "../Interfaces/Github/Contributors";
+import { Repository } from "../Interfaces/Github/Repository";
 import { IUser } from "../Interfaces/Users";
 import log from "../Lib/Logger";
 
@@ -7,27 +11,75 @@ export default class CacheClient
 {
     
     public User = new Map<IC_User, IUser>();
+    public Respositories = new Map<string, Repository>();
 
     constructor()
     {
         // Cache everything
         log.info(`Caching..`)
         // Cache user in database
-        this.CacheUser();
+        this.CacheGithub().then(async () => {
+            this.CacheUser();
+            setTimeout(() => {
+                console.log(this.User)
+            }, 5000)
+        });
+
     }
 
-    public CacheUser()
+    public async CacheUser()
     {
         UserModel.find().then(users => {
             users.forEach(user => {
+                let contributedto: Repository[] = [];
+                
+                for (const [key, value] of this.Respositories.entries()) {
+                    for(const contributor of value.contributors)
+                    {
+                        console.log(contributor.author.id + " == " +  user.github_id)
+                        if(contributor.author.id.toString() === user.github_id)
+                        {
+                            contributedto.push(value);
+                        }
+                    }
+                }
+                
                 this.User.set(user.github_id, {
                     discord_id: user.id,
                     discord_email: user.email,
                     email: user.email,
                     github_email: user.email,
-                    github_id: user.github_id
+                    github_id: user.github_id,
+                    contributedTo: contributedto
                 });
             });
         });
+    }
+
+    public async CacheGithub()
+    {
+        // Get all of our repos
+        const Repos = await (await fetch(`${Github_API}orgs/${Github_Org}/repos`, {
+            headers: {
+                authorization: `Basic ${Github_Client_Secret}`
+            }
+        })).json() as Array<any>;
+        console.log(Repos)
+        for await(let repo of Repos)
+        {
+            // Get contributors
+            const Contri = await (await fetch(`${Github_API}repos/${Github_Org}/${repo.name}/stats/contributors`, {
+                headers: {
+                    authorization: `Basic ${Github_Client_Secret}`
+                }
+            })).json() as Array<Contributor>
+
+            this.Respositories.set(repo.name, {
+                contributors: Contri,
+                name: repo.name,
+                owner: repo.owner.login
+            });
+        }
+        Promise.resolve(true);
     }
 }
---
+++ src/Interfaces/Cache/Cache_Contributors.ts
@@ -0,0 +1,3 @@
+import { Contributor } from "../Github/Contributors";
+
+export type IC_Contributor = Contributor["author"]["id"];
