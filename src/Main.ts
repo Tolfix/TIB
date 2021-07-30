4c5de341e9ca7fc6ac2de0dce17dab22a9ebffb5
Tolfix <matteus12122@gmail.com>
2021-07-30 16:35:31 +0200

    Modified < Main.ts >

--- src/Main.ts
+++ src/Main.ts
@@ -3,12 +3,17 @@ import CacheClient from "./Cache/Cache";
 import Check from "./Check";
 import Mongo_Database from "./Database/Mongo";
 import DiscordClient from "./Discord/Discord";
+import log from "./Lib/Logger";
 
 Check();
 
+new Mongo_Database();
+
+log.info(`Caching..`)
 CacheClient.CacheGithub().then(() => {
-    CacheClient.CacheUser()
+    CacheClient.CacheUser().then(() => {
+        log.info(`Caching done.`);
+        new DiscordClient();
+    });
 });
 
-new DiscordClient();
-new Mongo_Database();
