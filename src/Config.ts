71ea493cbd71c28a5b46427dfb45a821aee8c99e
Tolfx <matteus@tolfix.com>
2021-08-01 13:37:59 +0200

    Comments

--- src/Config.ts
+++ src/Config.ts
@@ -10,20 +10,31 @@ export const Discord_Ticket_Parent_Id = "870276061261336616"
 export const Discord_Member_Role_Id = "870570341024542781";
 export const Prefix = process.env.PREFIX ?? "tib ";
 
+/*
+* Express
+*/
 export const Express_Port = process.env.EXPRESS_PORT ?? 8080;
 export const Express_FQDN = process.env.FQDN ?? `localhost:${Express_Port}`;
 export const Express_HTTP: "https" | "http" = process.env.HTTP === "https" ? "https" : "http";
 export const Express_DOMAIN = `${Express_HTTP}://${process.env.FQDN ?? `localhost:${Express_Port}`}`;
 export const Express_Session_Secret = process.env.EXPRESS_SESSION_SECRET ?? require("crypto").randomBytes(20).toString("hex");
 
+/*
+* Database
+*/
 export const Database_Mongo_URI = process.env.DB_MONGO_URI ?? "mongodb://localhost/tib";
 
+/*
+* Github
+*/
 export const Github_Client_Id = process.env.GITHUB_CLIENT_ID ?? "";
 export const Github_Client_Secret = process.env.GITHUB_CLIENT_SECRET ?? "";
 export const Github_Secrets_Sponsorship = process.env.GITHUB_SECRETS_SPONSOR ?? "";
 export const Github_Org = "Tolfix";
 export const Github_API = "https://api.github.com/";
 
+/*
+* General
+*/
 export const HomeDir = ((__dirname.replace("\\build", "")).replace("/build", ""));
-
 export const DebugMode = process.env.DEBUG ?? false;
