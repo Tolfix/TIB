ae12f2dde1271640f4453c4b9704b6cd4a76594c
Matteus <tolfx@tolfix.com>
2021-08-01 20:46:03 +0200

    Modified button < Contributes >

--- src/Discord/Buttons/Information/Contributes.ts
+++ src/Discord/Buttons/Information/Contributes.ts
@@ -1,5 +1,7 @@
 import { MessageButton, MessageComponent } from "discord-buttons";
+import { MessageEmbed } from "discord.js";
 import CacheClient from "../../../Cache/Cache";
+import { Color_Green, Github_Org } from "../../../Config";
 import { ButtonIds } from "../../../Interfaces/Discord/ButtonsIds";
 import { Quick_Link_Github_Button } from "../../../Lib/Discord/QuickButton";
 import Logger from "../../../Lib/Logger";
@@ -14,6 +16,24 @@ export default class DeleteChannelButton extends Button
         if(!userCache)
             return button.reply.send(`Please ensure to link your account before checking.`, { ephemeral: true, component: Quick_Link_Github_Button })
     
-        return button.reply.send(`Coming soon...`, { ephemeral: true });
+        const user = CacheClient.User.get(userCache);
+        if(!user)
+            return;
+
+        if(!user.contributedTo)
+            return button.reply.send(`You haven't contributed to any repositories from \`${Github_Org}\`!`, { ephemeral: true });
+
+        let data = ``;
+        for(const contri of user.contributedTo)
+        {
+            data += `**${contri.owner}/${contri.name}** : ${contri.contributed.total}\n`
+        }
+
+        const embed = new MessageEmbed()
+        .setTitle(`Total contributes on each repository`)
+        .setDescription(data)
+        .setColor(Color_Green);
+
+        return button.reply.send(embed, { ephemeral: true });
     }
 }
