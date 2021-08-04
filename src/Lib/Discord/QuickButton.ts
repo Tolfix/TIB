c013bfed04789e83e4f5ce6c0001f0924c04de2a
Tolfx <matteus@tolfx.dev>
2021-08-04 10:08:35 +0200

    Added quick buton

--- src/Lib/Discord/QuickButton.ts
+++ src/Lib/Discord/QuickButton.ts
@@ -1,3 +1,4 @@
+import { Github_Org } from "../../Config";
 import CreateButton from "./CreateButton";
 
 export const Quick_Link_Github_Button = CreateButton({
@@ -5,3 +6,11 @@ export const Quick_Link_Github_Button = CreateButton({
     emoji: `🔗`,
     url: "https://tib.tolfix.com/oauth2/link"
 });
+
+export const Quick_Sponsor_Github_Button = CreateButton(
+    {
+        name: "Sponsor us!",
+        emoji: "🎊",
+        url: `https://github.com/sponsors/${Github_Org}`
+    }
+)
